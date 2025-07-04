2edf53f08afdc93dee5fedbd96ca16069e0dba0a
Farai <fabvu@Farais-MacBook-Pro.local>
2025-07-04 19:52:56 -0300

    update config file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env variables from .env file in the root directory.
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
+    base: "/",
     build: {
-      outDir: 'dist',
+      outDir: "dist",
     },
     server: {
       port: 3000,
